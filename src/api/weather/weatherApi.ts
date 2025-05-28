de39fad20b93664c7566fb2e38b52f5fa4281df6
Mukhin001 <purgen.igor@yandex.ru>
2025-05-28 23:05:41 +0300

    28vercel

--- src/api/weather/weatherApi.ts
+++ src/api/weather/weatherApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const  weatherApi = createApi({
     reducerPath: 'weatherApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://api.openweathermap.org'}),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://api.openweathermap.org'}),
     tagTypes: ['Weather'],
     endpoints: () => ({}),
 });
